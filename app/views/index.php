b4639e39eb733b603f4e86ba226fee68a6035826
kandirou0 <simo.kandirou15@gmail.com>
2022-12-04 17:54:06 +0100

    update

--- app/views/index.php
+++ app/views/index.php
@@ -7,5 +7,4 @@
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veniam deleniti tenetur commodi sint ex, molestias in doloribus nobis excepturi odio tempore quidem totam assumenda culpa maiores quam inventore aperiam!</p><br> 
 </div>
 
-
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
